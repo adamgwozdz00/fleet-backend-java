d13eb8a1ea4f2969730b0ab8beb9a6d38900943e
adamgwozdz00 <adamgwozdz00@gmail.com>
2022-11-08 08:20:44 +0100

    aspects

--- common/src/main/java/pl/ag/fleet/timemeasure/TimerAspect.java
+++ common/src/main/java/pl/ag/fleet/timemeasure/TimerAspect.java
@@ -2,6 +2,7 @@ package pl.ag.fleet.timemeasure;
 
 import java.util.logging.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -16,11 +17,14 @@ public class TimerAspect {
   private final static Logger LOGGER = Logger.getLogger(TimerAspect.class.getName());
 
   @Around("@annotation(pl.ag.fleet.timemeasure.Timer)")
-  private void beforeTimerAnnotation(JoinPoint joinPoint) {
+  private Object beforeTimerAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
     var startTime = System.currentTimeMillis();
+    var join = joinPoint.proceed();
+    var endTime = System.currentTimeMillis();
     LOGGER.info(joinPoint.getSignature().getDeclaringTypeName() + " :: executed in : " + (
-        System.currentTimeMillis()
+        endTime
             - startTime) + " ms");
+    return join;
   }
 
 }

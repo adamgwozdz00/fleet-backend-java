4cbfbee950f74f351c05735149bc4d0b3c96dfe4
adamgwozdz00 <adamgwozdz00@gmail.com>
2022-11-15 18:16:38 +0100

    Add company table to db

--- bl/src/main/java/pl/ag/fleet/company/CompanyProvider.java
+++
@@ -1,5 +0,0 @@
-package pl.ag.fleet.company;
-
-public interface CompanyProvider {
-
-}
---
+++ flyway/src/main/resources/db/migration/V3.0.0__create_company.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS fleet.company
+(
+    id         bigint,
+    name       character varying(255)
+);

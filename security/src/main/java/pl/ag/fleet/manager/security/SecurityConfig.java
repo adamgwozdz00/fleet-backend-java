108094db528a5eb84e85662a6aaab443adfe4d85
adamgwozdz00 <adamgwozdz00@gmail.com>
2022-11-28 20:21:11 +0100

    improve cors configs

--- security/src/main/java/pl/ag/fleet/manager/security/SecurityConfig.java
+++ security/src/main/java/pl/ag/fleet/manager/security/SecurityConfig.java
@@ -1,16 +1,23 @@
 package pl.ag.fleet.manager.security;
 
+import java.util.Arrays;
+import java.util.Collections;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,12 +39,14 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
         .csrf().disable()
-        .cors().disable()
+        .cors().configurationSource(corsConfigurationSource())
+        .and()
         .authorizeRequests()
         .antMatchers("/login", "/swagger-ui/**", "/v2/api-docs", "/configuration/ui",
             "/swagger-resources/**", "/configuration/**", "/swagger-ui.html"
             , "/webjars/**", "/csrf", "/").permitAll()
-        .anyRequest().authenticated()
+        .anyRequest()
+        .authenticated()
         .and()
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -46,14 +55,31 @@ public class SecurityConfig {
         .build();
   }
 
+
+
   @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(allowedOrigin);
-      }
-    };
+  public CorsConfigurationSource corsConfigurationSource() {
+    val corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedOrigins(Collections.singletonList(allowedOrigin));
+    corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+    corsConfiguration.setExposedHeaders(Collections.singletonList("*"));
+    val source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return source;
   }
+
+//  @Bean
+//  public WebMvcConfigurer corsConfigurer() {
+//    return new WebMvcConfigurer() {
+//      @Override
+//      public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins(allowedOrigin)
+//            .allowedHeaders("*")
+//            .allowedMethods("*");
+//      }
+//    };
+//  }
 
 }
